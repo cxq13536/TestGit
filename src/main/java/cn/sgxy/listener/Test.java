d3f0daa07300d7d97e66705614d8a70a6afcaf27
eclipse_xq <eclipse_xq@qq.com>
2019-06-24 20:14:05 +0800

    edit by hot_fix

--- src/main/java/cn/sgxy/listener/Test.java
+++ src/main/java/cn/sgxy/listener/Test.java
@@ -4,5 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		System.out.println("left test ....");
 		System.out.println("right test ....");
+		System.out.println("hot_fix");
 	}
 }
